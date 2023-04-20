888410d0dafcc0281770cadbaf1935716c7e3771
Devrock2847 <93879277+Devrock2847@users.noreply.github.com>
2023-04-20 13:31:52 +0100

    Added comments

--- 2023Assignment/Source.cpp
+++ 2023Assignment/Source.cpp
@@ -8,38 +8,55 @@
 #include <thread>
 
 int runGame() {
+	//gameCounter will total the amount of games played 
 	int gameCounter = 0; 
+	//gameScore will total the amount of games won by the user 
 	int gameScore = 0; 
+	//timedOut is generic boolean required for the 10s timeout function
 	bool timedOut = false;
+	//scoreBoard is a generic variable that will be populated with the viewScoreBoard function
 	std::vector<std::pair<double, std::string>> scoreBoard;
+	//timeKeeper stores the times the user takes to answer a question
 	std::vector<float> timeKeeper;
-
+	//declaration of the filenames used within the project structure
 	std::vector<std::string> files = { "Alps.txt", "Carpathians.txt", "Icelandic Highlands.txt", "Pyrenees.txt" };
+	//creates an object of the Mountains class
 	Mountains mountainObject(files); 
+	//creates an object of the Calculations class
 	Calculations calculationObject;
+	//calls vectorTrim to take the filenames and remove the filetype from them and place them into a new vector with the values; { "Alps", "Carpathians", "Icelandic Highlands", "Pyrenees" }
 	std::vector<std::string> mountainRange = calculationObject.vectorTrim(files);
-
+	//while loop put the game in infinite loop unless the user types 'quit' or 'exit'
 	while (true) { 
+		//generic variables that will be used later 
 		bool isTrue;
 		std::string userInput;
+		//calls getRandomMountain from the Mountains class to give a random mountain from the 4 text files
 		std::string randomMountain = mountainObject.getRandomMountain();
+		//Presents the user with the question of where the mountain belongs
 		std::cout << "\n";
 		std::cout << "Which mountain range does the following mountain belong too: " << randomMountain << "\n";
+		//presents the user with 4 different mountain ranges calling from the mountainRange variable initialised on line 28
 		std::cout << "[1]" << mountainRange[0] << "   " << "[2]" << mountainRange[1] << "   " << "[3]" << mountainRange[2] << "   " << "[4]" << mountainRange[3] << std::endl;
+		//The program tracks how quickly a user answers the question, the timer starts with this line
 		std::chrono::steady_clock::time_point t = std::chrono::steady_clock::now();
+		//inputReceived remains false untill the user has submitted there answer 
 		bool inputReceived = false;
+		//starts the 10s timeout counter at 0
 		time_t startTime = time(NULL);
+		//this is the timeout number, if we wanted it to be a 20s timeout we would change this variable
 		time_t waitTime = 10;
-		
+		//a thread asks for the userInput(to answer the question) and changes inputReceieved to true when the input is received therby restarting the loop
 		std::thread t1([&]() {
 			std::cin >> userInput;
 		inputReceived = true;
 			});
 		t1.detach();
-		// check the inputReceived flag once every 50ms for 10 seconds
+		// check the inputReceived flag once every 50ms for 10 seconds 
 		while (time(NULL) < startTime + waitTime && !inputReceived) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(50));
 		}
+		//if the user inputs an answer within the 10s this code runs
 		if (inputReceived) {
 			long double elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - t).count() / 1000000000.0;
 			timeKeeper.push_back(elapsed);
@@ -77,6 +94,7 @@ int runGame() {
 				std::cout << "Invalid input please enter a 1,2,3,4 or type 'quit' or 'exit' or 'score'" << std::endl;
 			}
 		}
+		//if the user did not input an answer before the 10s timeout this code will run telling them they were too slow and adding the incorrect answer to the total
 		else {
 			gameCounter = gameCounter + 1;
 			std::cout << "\n";
@@ -87,10 +105,12 @@ int runGame() {
 }
 
 int main() {
+	//Explaining the rules to the game prior to the main while loop
 	std::cout << "Welcome to RangeCheck, a quiz game to test your knowledge about mountains!" << std::endl;
 	std::cout << "Rules are simple you will be presented with a mountain and you must select the range it belongs too." << std::endl;
 	std::cout << "Enter 1,2,3,4 to answer or enther 'exit' or 'quit' to stop playing" << std::endl;
 	std::cout << "Enter 'score' to view your fastest times and if you take longer than 10 seconds your answer will be marked incorrect" << std::endl;
 	std::cout << "\n";
+	//calls for the game to be run 
 	runGame();
 }
