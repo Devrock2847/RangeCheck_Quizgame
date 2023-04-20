881004d2492bce3b9735d1f8dfdcbb9801d170a2
Devrock2847 <93879277+Devrock2847@users.noreply.github.com>
2023-04-20 15:06:47 +0100

    Mountains.h commented

--- 2023Assignment/Mountains.h
+++ 2023Assignment/Mountains.h
@@ -4,10 +4,12 @@
 #include<map>
 
 class Mountains {
+	//declares variables used throughout the class including the main data struture the map
 	std::vector<std::string> vectorious;
 	std::map < std::string, std::vector<std::string> > data;
 	std::string fileType;
 public:
+	//declares the functions detailed in the Mountains.cpp file
 	Mountains(std::vector<std::string>& filenames);
 	std::string getRandomMountain();
 	bool checkRange(std::string mountain, std::string range);
